98d3e605a5d5ae8ed48b16d932c5855d0bb6267d
Cale Shapera <25466659+cshape@users.noreply.github.com>
2025-12-28 13:53:01 -0800

    link style and remove title page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
 import About from './pages/About'
 import Chat from './pages/Chat'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/" element={<About />} />
       <Route path="/chat" element={<Chat />} />
       <Route path="*" element={<About />} />
     </Routes>
